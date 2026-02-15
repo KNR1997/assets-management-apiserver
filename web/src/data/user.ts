a69f237e683aa70b44b57295eb04b802a8e32ddc
KNR1997 <kethakaranasinghe@gmail.com>
2026-02-15 23:25:06 +0530

    feat: update userStore after profile update

--- web/src/data/user.ts
+++ web/src/data/user.ts
@@ -2,16 +2,21 @@ import { useMessage } from 'naive-ui'
 import { userClient } from './client/user'
 import { API_ENDPOINTS } from './client/api-endpoints'
 import { useMutation, useQueryClient } from '@tanstack/vue-query'
+import { useUserStore } from '@/store'
 
 export const useUpdateProfile = () => {
   const queryClient = useQueryClient()
   const message = useMessage()
+  const userStore = useUserStore()
 
   return useMutation({
     mutationFn: userClient.update,
-    onSuccess: () => {
+    onSuccess: async () => {
       message.success('Updated successfully')
-
+      // fetch user detail (username, email)
+      const me = await fetchMe()
+      // store details global state
+      userStore.setUserInfo(me.data)
       queryClient.invalidateQueries({
         queryKey: [API_ENDPOINTS.PROFILE],
       })
